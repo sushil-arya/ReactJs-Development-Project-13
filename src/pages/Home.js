dba0745435535591aca52b9ed552f9081f89bde4
sushil-arya <sushilarya888@gmail.com>
2024-03-25 01:47:34 +0530

    bootstrap2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
-import { Row,Col } from "react-bootstrap";
+import { Row,Col, Nav } from "react-bootstrap";
 
     export default class Home extends Component {
     render() {
         return (
         <>
-            <header>Header</header>
+            <header>
+                <Nav activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+                    <Nav.Item>
+                        <Nav.Link href="/home">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-1">Login</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="link-2">Register</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+
+            </header>
             <main>
                 <Row className="h-100">
                     <Col sm={2}>
                         <aside>Aside</aside>
                     </Col>
                     <Col sm={10}>
-                        <section>Section</section>
+                        <section>Home  Section</section>
                     </Col>
                 </Row>
             </main>
